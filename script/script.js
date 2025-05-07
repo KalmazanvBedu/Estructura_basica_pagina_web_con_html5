622c2fb4ef667aba58ef96348b4e8dfa4cbc954e
Keny 1 <keny.almazan@mrm.com>
2025-05-06 21:36:19 -0600

    Commit 2 - Se agrega Tag Manager

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const contenedor_titulo_principal = document.getElementById("contenedor-titulo-principal");
+
+contenedor_titulo_principal.addEventListener("click", () => {
+    // gtag('event','button_click',{
+    //     'buttonColor': 'purple'
+    // });
+})
+
+window.addEventListener("load", () => {
+    // gtag('event', 'page_view',{
+    //     'tipoPagina': 'informativa'
+    // });
+})
